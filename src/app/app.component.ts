0d44d59b95b94ba6c620e2464ef18c2fe487c1ef
Майринова Марина <mayrinova@evolenta.ru>
2024-07-22 15:21:24 +0300

    Практическое задание5: Модули

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'project_learn_5';
+export class AppComponent implements OnInit {
+  title = 'Модули';
+  ngOnInit(){
+  }
 }
